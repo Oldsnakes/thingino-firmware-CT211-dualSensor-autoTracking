55ea1ea6c03732edc850a68fea577a930c8004ef
Oldsnakes <yswey@aol.com>
2026-01-31 17:33:14 -0800

    Add files via upload

---
+++ ingenic-sdk/3.10/sensor-src/include/sensor-info.h
@@ -0,0 +1,23 @@
+#ifndef SENSOR_INFO_H
+#define SENSOR_INFO_H
+
+struct sensor_info {
+	const char *name;
+	unsigned int chip_id;
+	const char *version;
+	int min_fps;
+	int max_fps;
+	int actual_fps;
+	unsigned int chip_i2c_bus;
+	unsigned int chip_i2c_addr;
+	int width;
+	int height;
+};
+
+void sensor_common_init(struct sensor_info *info);
+void sensor_common_exit(void);
+void sensor1_common_init(struct sensor_info *info);
+void sensor1_common_exit(void);
+void sensor_update_actual_fps(int fps);
+
+#endif // SENSOR_INFO_H
